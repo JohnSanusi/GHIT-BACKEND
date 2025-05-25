a8424d4b9990d7feb5e3431f0193583850c933ff
JohnSanusi <johnsanusi18@gmail.com>
2025-05-25 09:15:22 +0100

    integrated cors in server

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 import express from "express";
+import cors from "cors";
 import courses from "./routes/courses.js";
 import prodcuts from "./routes/products.js";
 const port = process.env.PORT;
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://ghit.vercel.app",
+  "https://ghit-admin.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("not alowd by CORS"));
+      }
+    },
+  })
+);
+
+app.use(express.json());
 app.use("/api/courses", courses);
 app.use("/api/products", prodcuts);
 app.listen(port, () => console.log(`server is running on port ${port}`));
