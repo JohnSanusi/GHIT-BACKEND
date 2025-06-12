7e41bafb0589e248f0687dac6a8153fdb9667aaf
JohnSanusi <johnsanusi18@gmail.com>
2025-06-12 11:16:37 +0100

    added uploads directory

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,9 +1,14 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    const uploadPath = path.join(process.cwd(), "uploads");
+    if (!fs.existsSync(uploadPath)) {
+      fs.mkdirSync(uploadPath);
+    }
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const ext = path.extname(file.originalname);
